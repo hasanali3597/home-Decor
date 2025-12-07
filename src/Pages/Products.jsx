a6bcd2378729c197e92d855c5f1e6dc0e71c3813
hasanali3597 <hasanali3597@gmail.com>
2025-12-07 16:37:26 +0600

    search add

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import useProducts from "../hooks/useProducts";
 import ProductCard from "../Components/ProductCard";
 
 const Products = () => {
   const { products } = useProducts();
+  const [search, setSearch] = useState("");
+
+  const tram = search.trim().toLocaleLowerCase();
+  const searchProductas = tram
+    ? products.filter((product) =>
+        product.name.toLocaleLowerCase().includes(tram)
+      )
+    : products;
+  console.log(searchProductas);
   return (
     <div>
       <div className="flex justify-between py-5 items-center ">
         <h1 className="text-2xl font-semibold">
-          Feactured Producta : <span>{products.length}</span>
+          Feactured Producta : <span>{searchProductas.length}</span>
         </h1>
-        <button className="btn btn-secondary">All Producta</button>
+        <label className="input">
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            type="search"
+            placeholder="Search Products"
+          />
+        </label>
       </div>
       <div className="grid  text-center md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {products.map((product) => (
+        {searchProductas.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
